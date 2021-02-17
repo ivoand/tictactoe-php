59b9add98551ec737aeb5e3ec9539ea1d3284842
chanderslv <chanderslv@gmail.com>
2021-02-17 20:10:24 +0200

    start

---
+++ index.php
@@ -0,0 +1,13 @@
+<link rel="stylesheet" href="style.css">
+
+<div class="container">
+    <a href="#"></a>
+    <a href="#"></a>
+    <a href="#"></a>
+    <a href="#"></a>
+    <a href="#"></a>
+    <a href="#"></a>
+    <a href="#"></a>
+    <a href="#"></a>
+    <a href="#"></a>
+</div>
