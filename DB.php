6ec5ceb0c1d1e7b5653d2410d74514837a79926c
chanderslv <chanderslv@gmail.com>
2021-02-17 21:58:40 +0200

    update

---
+++ DB.php
@@ -0,0 +1,52 @@
+<?php
+class DB 
+{
+    private $con;
+    private $table;
+
+    public function __construct($table_name) {
+
+        $this->table = $table_name;
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "tictactoe";
+
+        // Create connection
+        $this->con = new mysqli($servername, $username, $password, $dbname);
+        // Check connection
+        if ($this->con->connect_error) {
+            die("Connection failed: " . $this->con->connect_error);
+        }
+    }
+
+    public function __destruct() {
+        $this->con->close();
+    }
+
+
+    public function set($entries) {
+        $columns = '';
+        $values = '';
+        $i = 0;
+        foreach ($entries as $key => $value) {
+          $columns .= "`$key`";
+          $values .= "'$value'";
+          $i++;
+          if (count($entries) > $i) {
+            $columns .= ", ";
+            $values .= ", ";
+          }
+        }
+  
+        $sql = "INSERT INTO `$this->table` ($columns) VALUES ($values);";
+        $result = $this->con->query($sql);
+        if ($result === true) {
+          return $this->con->insert_id;
+        }
+        else {
+          echo $this->con->error;
+        }
+      }
+    
+}
--- index.php
+++ index.php
@@ -1,13 +1,24 @@
 <link rel="stylesheet" href="style.css">
 
-<div class="container">
-    <a href="#"></a>
-    <a href="#"></a>
-    <a href="#"></a>
-    <a href="#"></a>
-    <a href="#"></a>
-    <a href="#"></a>
-    <a href="#"></a>
-    <a href="#"></a>
-    <a href="#"></a>
-</div>
+<?php
+include 'DB.php';
+$game = new DB('game')
+?>
+
+<?php if (array_key_exists('game-id', $_REQUEST)) { ?>
+    <div class="container">
+        <?php for ($c = 1; $c <= 3; $c++) : ?>
+            <?php for ($r = 1; $r <= 3; $r++) : ?>
+                <a href="?c=<?= $c ?>&r=<?= $r ?>">-</a>
+            <?php endfor; ?>
+        <?php endfor; ?>
+
+    </div>
+
+<?php
+
+} else { ?>
+ <a class="btn" href="/tictactoe-php/startgame.php">start game</a>
+<?php
+}
+?>
